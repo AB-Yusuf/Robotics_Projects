8ad369eba20b5ab333a5b1fd5c2eaaa7e9c8aa36
Abubakar <45397477+AB-Yusuf@users.noreply.github.com>
2019-09-16 12:02:09 +0100

    Project_Blink_RGB_LED
    An RGB LED is made to display different colors with a time delay of 1 second.
    Colors displayed includes: Red, Green, Blue, Indigo, Purple.

---
+++ BlinkRGB_LED_sep16c/BlinkRGB_LED_sep16c.ino
@@ -0,0 +1,48 @@
+int redPin = 11;
+int greenPin = 10;
+int bluePin = 9;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  //Set each pin mode to give an output of 5V or 3.5V
+
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+
+}
+
+void setColor(int red, int green, int blue) {
+  // put your main code here, to run repeatedly:
+
+  //AnalogWrite can be used to light Led at varying brightnesses
+  //The function takes the value for brightness
+  // The value range: 0 - 255
+  
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
+  
+  
+}
+
+void loop()
+{
+  //Entering the values for each color as an argument
+  setColor(255, 0, 0); //red
+  delay(1000);
+  setColor(0, 255, 0); //green
+  delay(1000);
+  setColor(0, 0, 255); // blue
+  delay(1000);
+  setColor(255, 255, 0); //yellow
+  delay(1000);
+  setColor(80, 0, 80);  //purple
+  delay(1000);    
+  setColor(0, 255, 255); //aqua 
+  delay(1000);
+  setColor(0x4B, 0x0, 0x82); 
+  delay(1000);
+  delay(1000);
+}
