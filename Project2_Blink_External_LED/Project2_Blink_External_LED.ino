bb33c3650e310369c0f0676d74de299e98bc5f6c
Abubakar <45397477+AB-Yusuf@users.noreply.github.com>
2019-10-02 11:00:41 +0100

    Project2_Blink_External_LED
    The project was repeated to test for mastery and understanding of the structure.
    External instruments used: Resistor and an LED
    The LED was turned ON for the first five seconds 
    Then LED was turned OFF for the next five seconds,
    This is repeated continuously till the program ends.

---
+++ Project2_Blink_External_LED/Project2_Blink_External_LED.ino
@@ -0,0 +1,17 @@
+
+int led = 10;
+
+// the setup function runs once when you press reset or power the board
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(led, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(5000);                       // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(5000);                       // wait for a second
+}
